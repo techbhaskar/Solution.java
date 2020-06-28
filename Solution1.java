d8f832ee9bcff36cf08156a7edffb2b184abf43a
sphotow1 <67553260+sphotow1@users.noreply.github.com>
2020-06-28 13:47:04 -0400

    Testing
    testing

---
+++ Solution1.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Scanner;
+
+public class Solution1 {
+	    
+	    public static void main(String args[]) {
+	       System.out.println("Hellow WOrld!!!");	        
+	    }
+	}
