117b8f34f36ea689ea9c853b696ff105f6076aa1
Arani Bhaskararao <bhaskara.ee@gmail.com>
2020-06-28 22:47:34 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Scanner;
+
+public class Solution {
+	    private static final Scanner scan = new Scanner(System.in);
+	    
+	    static  String readFile(String fileName) throws IOException {
+
+	        BufferedReader br = new BufferedReader(new FileReader(fileName));
+
+	        try {
+
+	            StringBuilder sb = new StringBuilder();
+
+	            String line = br.readLine();
+	            while (line != null) {
+	                String[] temp = line.split(" ");
+	                String tempRes = temp[6];
+	                String lastWord = tempRes.substring(tempRes.lastIndexOf("/") + 1);
+	                
+	                if(lastWord.contains(".gif")){
+	                    sb.append(lastWord);
+	                    sb.append("\n");
+	                }
+	                
+
+	               
+
+	                line = br.readLine();
+
+	            }
+	            System.out.println(sb.toString());
+	            return sb.toString();
+
+	        } finally {
+
+	            br.close();
+
+	        }
+
+	    }
+	    
+	    public static void main(String args[]) throws Exception {
+	        // read the string filename
+	        String filename;
+	        //filename = "testss.txt";
+	        filename = scan.next();   
+	        OutputStream os = null;
+	        String data = readFile(filename);
+	        String resultFileName = "gifs_"+filename;
+
+	        try {
+
+	            // below true flag tells OutputStream to append
+
+	            os = new FileOutputStream(new File(resultFileName), true);
+
+	            os.write(data.getBytes(), 0, data.length());
+
+	        } catch (IOException e) {
+
+	            e.printStackTrace();
+
+	        } finally {
+
+	            try {
+
+	                os.close();
+
+	            } catch (IOException e) {
+
+	                e.printStackTrace();
+
+	            }
+
+	        }
+	        
+	    }
+	}
