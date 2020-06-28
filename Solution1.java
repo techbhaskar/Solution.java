238d951e1ef1edb422137daae6d3efe7d2453bb9
Arani Bhaskararao <barani@breakthroughss.com>
2020-06-28 23:19:07 +0530

    Test
    test

---
+++ Solution1.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Scanner;
+
+public class Solution {
+	    
+	    public static void main(String args[]) {
+	        System.out.println("Hello world!!!!!!!!!!!");
+	        
+	    }
+	}
