5e990e92e84c51b56e9c1bf53e19c6c1ae0b8d86
Arani Bhaskararao <barani@breakthroughss.com>
2020-06-28 23:28:43 +0530

    asd
    asdasd

--- Solution1.java
+++ Solution1.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class Solution {
 	    
 	    public static void main(String args[]) {
-	        System.out.println("Hello world!!!!!!!!");
+	        System.out.println("Hello world!!!!!!");
 	        
 	    }
 	}
